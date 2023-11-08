5b43d15ce8e9812672861add03db962fc1b29a8e
Wyatt Shinkle <wyattshink@gmail.com>
2023-11-08 15:06:12 -0500

    need to analyze different json structs because they vary slightly

--- parse.py
+++ parse.py
@@ -62,25 +62,70 @@ class Database:
                 if f.endswith('.json'):
                     with open(file_path, 'r') as cve_file:
                         cve_json = json.load(cve_file)
-                        cve_id = cve_json['containers']['cna']['x_legacy_V4Record']['CVE_data_meta']['ID']
-                        cve_title = cve_json['containers']['cna']['x_legacy_V4Record']['CVE_data_meta']['TITLE']
-                        cve_desc = cve_json['containers']['cna']['x_legacy_V4Record']['description']['descriptiondata'][0]['value']
-                        cve_attack = cve_json['containers']['cna']['x_legacy_V4Record']['impact']['cvss']['attackComplexity']
-                        cve_availability = cve_json['containers']['cna']['x_legacy_V4Record']['impact']['cvss']['availabilityImpact']
-                        cve_base_score = cve_json['containers']['cna']['x_legacy_V4Record']['impact']['cvss']['baseScore']
-                        cve_base_severity = cve_json['containers']['cna']['x_legacy_V4Record']['impact']['cvss']['baseSeverity']
-                        cve_confidentiality = cve_json['containers']['cna']['x_legacy_V4Record']['impact']['cvss']['confidentialityImpact']
-                        cve_privileges = cve_json['containers']['cna']['x_legacy_V4Record']['impact']['cvss']['privilegesRequired']
-                        cve_discovery = cve_json['containers']['cna']['x_legacy_V4Record']['source']['discovery']
-                        cve_date = cve_json['containers']['cna']['x_legacy_V4Record']['CVE_data_meta']['DATE_PUBLIC']
-                        cve_references = cve_json['containers']['cna']['x_legacy_V4Record']['references']['reference_data']
+                        try:
+                            cve_id = cve_json['containers']['cna']['x_legacyV4Record']['CVE_data_meta']['ID']
+                        except KeyError:
+                            cve_id = 'NULL'
+                        try:
+                            cve_title = cve_json['containers']['cna']['x_legacyV4Record']['CVE_data_meta']['TITLE']
+                        except KeyError:
+                            cve_title = 'NULL'
+                        try:
+                            cve_desc = cve_json['containers']['cna']['x_legacyV4Record']['description']['descriptiondata'][0]['value']
+                        except KeyError:
+                            cve_desc = 'NULL'
+                        try:
+                            cve_attack = cve_json['containers']['cna']['x_legacyV4Record']['impact']['cvss']['attackComplexity']
+                        except KeyError:
+                            cve_attack = 'NULL'
+                        except TypeError:
+                            print(file_path)
+
+
+                        try:
+                            cve_availability = cve_json['containers']['cna']['x_legacyV4Record']['impact']['cvss']['availabilityImpact']
+                        except KeyError:
+                            cve_availability = 'NULL'
+                        try:
+                            cve_base_score = cve_json['containers']['cna']['x_legacyV4Record']['impact']['cvss']['baseScore']
+                        except KeyError:
+                            cve_base_score = 'NULL'
+                        try:
+                            cve_base_severity = cve_json['containers']['cna']['x_legacyV4Record']['impact']['cvss']['baseSeverity']
+                        except KeyError:
+                            cve_base_severity = 'NULL'
+                        try:
+                            cve_confidentiality = cve_json['containers']['cna']['x_legacyV4Record']['impact']['cvss']['confidentialityImpact']
+                        except KeyError:
+                            cve_confidentiality = 'NULL'
+                        try:
+                            cve_privileges = cve_json['containers']['cna']['x_legacyV4Record']['impact']['cvss']['privilegesRequired']
+                        except KeyError:
+                            cve_privileges = 'NULL'
+                        try:
+                            cve_discovery = cve_json['containers']['cna']['x_legacyV4Record']['source']['discovery']
+                        except KeyError:
+                            cve_discovery = 'NULL'
+                        try:
+                            cve_date = cve_json['containers']['cna']['x_legacyV4Record']['CVE_data_meta']['DATE_PUBLIC']
+                        except KeyError:
+                            cve_date = 'NULL'
+                        try:
+                            cve_references = cve_json['containers']['cna']['x_legacyV4Record']['references']['reference_data']
+                        except KeyError:
+                            cve_references = []
                         connection = sqlite3.connect(self.file)
                         cursor = connection.cursor()
                         cursor.execute('''INSERT INTO cve VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', 
                         (cve_id, cve_title, cve_desc, cve_attack, cve_availability, 
                         cve_base_score, cve_base_severity, cve_confidentiality, cve_privileges, cve_discovery, cve_date))
                         for ref in cve_references:
-                            cursor.execute('''INSERT INTO cve_references VALUES (?, ?)''', (cve_references['url'], cve_id))
+                            try:
+                                cursor.execute('''INSERT INTO cve_references VALUES (?, ?)''', (ref['url'], cve_id))
+                            except KeyError:
+                                pass
+                        connection.commit()
+                        connection.close()
 
 
 if __name__ == "__main__":
